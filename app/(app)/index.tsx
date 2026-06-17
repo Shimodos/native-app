c0db2b654cf818b649ac3896ee6017bf1297b578
Aleksey Demchenko <ad@pesochin.net>
2026-06-17 19:49:22 +0300

    9.5. atomWithStorage

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -2,20 +2,9 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Colors, Fonts } from '../../shared/tokens';
 import { useAtom } from 'jotai';
 import { profileAtom } from '../../entities/user/model/user.state';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect } from 'react';
 
 export default function MyCoursePage() {
 	const [profile] = useAtom(profileAtom);
-
-	useEffect(() => {
-		AsyncStorage.setItem('demo', 'test').then(async () => {
-			console.log(await AsyncStorage.getAllKeys());
-			console.log(await AsyncStorage.getItem('demo'));
-			console.log(await AsyncStorage.removeItem('demo'));
-			console.log(await AsyncStorage.getItem('demo'));
-		});
-	}, []);
 
 	return (
 		<View>
