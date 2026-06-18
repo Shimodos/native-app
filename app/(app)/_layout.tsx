166bb78054eabbb41cbbf261867b9170b4e9ceda
Aleksey Demchenko <ad@pesochin.net>
2026-06-18 18:57:25 +0300

    9.10. Redirect компонент

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,16 +1,16 @@
-import { SplashScreen, Stack } from 'expo-router';
-// const navTheme = {
-// 	...DarkTheme,
-// 	colors: {
-// 		...DarkTheme.colors,
-// 		background: Colors.black,
-// 		card: Colors.black,
-// 	},
-// };
+import { Redirect, SplashScreen, Stack } from 'expo-router';
+import { loginAtom } from '../../entities/auth/model/auth.state';
+import { useAtomValue } from 'jotai';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function AppLayout() {
+	const { access_token } = useAtomValue(loginAtom);
+
+	if (!access_token) {
+		return <Redirect href="/login" />;
+	}
+
 	return (
 		<Stack>
 			<Stack.Screen name="index" />
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,21 +1,7 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { Colors, Fonts } from '../../shared/tokens';
-import { useAtomValue } from 'jotai';
-import { loginAtom } from '../../entities/auth/model/auth.state';
-import { useEffect } from 'react';
-import { router, useRootNavigationState } from 'expo-router';
 
 export default function MyCoursePage() {
-	const { access_token } = useAtomValue(loginAtom);
-	const state = useRootNavigationState();
-
-	useEffect(() => {
-		if (!state?.key) return;
-		if (!access_token) {
-			router.replace('/login');
-		}
-	}, [access_token]);
-
 	return (
 		<View>
 			<Text style={styles.textStyle}>index</Text>
