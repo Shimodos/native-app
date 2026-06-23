70fed52004c3b1f4825c4f97c722e758f2843b86
Aleksey Demchenko <ad@pesochin.net>
2026-06-24 00:44:44 +0300

    10.4. Кастомный Drawer

--- entities/layout/ui/CustomDrawer/CustomDrawer.tsx
+++ entities/layout/ui/CustomDrawer/CustomDrawer.tsx
@@ -1,5 +1,5 @@
 import { DrawerContentComponentProps, DrawerContentScrollView } from '@react-navigation/drawer';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Colors } from '../../../../shared/tokens';
 import { CustomLink } from '../../../../shared/CustomLink/CustomLink';
 
